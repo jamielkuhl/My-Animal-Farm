6f1a9113fa9a797c2fe86e10fc0ab81f496242bd
agmoroz <amoroz@macmini-lab7.local>
2015-03-11 10:18:43 -0400

    I FINISHED

--- Farm.java
+++ Farm.java
@@ -4,5 +4,16 @@ public class Farm
   {
     World wref = new World();
     JamiePenguin ex1 = new JamiePenguin(wref);
+    ExampleGiraffe nick = new ExampleGiraffe(wref);
+    
+    nick.forward(-90);
+    nick.turn(45);
+    nick.forward(100);
+    nick.turn(90);
+    nick.turn(-45);
+    nick.backward(100);
+    nick.turn(-45);
+    nick.backward(100);
+    nick.turn(-45);
   }
 }
